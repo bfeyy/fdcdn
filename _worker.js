732f19e3d177b0d46ca91a882afd30207e60ef24
bfeyy <161719715+bfeyy@users.noreply.github.com>
2024-03-02 17:20:16 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'fd.qq123zzp.workers.dev'
+      url.hostname = 'm.ip138.com'
       let new_request = new Request(url, request);
       return fetch(new_requestbfvyy.dynv6.net);
     }
