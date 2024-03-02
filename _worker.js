41d8ef82fdd8b9406484bce0e59dc7549e0d10a5
bfeyy <161719715+bfeyy@users.noreply.github.com>
2024-03-02 17:21:54 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'm.ip138.com'
+      url.hostname = 'benevolent-mochi-4738c9.netlify.app'
       let new_request = new Request(url, request);
       return fetch(new_requestbfvyy.dynv6.net);
     }
